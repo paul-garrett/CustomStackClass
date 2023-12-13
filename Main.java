4d5ddb4633ea0836b88aa80e02892b0581caebef
Paul Garrett <89760819+paul-garrett@users.noreply.github.com>
2023-12-13 14:28:41 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+	public static void main(String[] args) {
+		Stack testStack = new Stack();
+    System.out.println(testStack.size());
+    System.out.println(testStack.isEmpty());
+	}
+}
